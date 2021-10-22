856ae03a803064a12c70f7638ca5818733af3d69
Jordan McGhee <jordanmcghee95@gmail.com>
2021-10-22 18:29:45 -0400

    Added form to submit new meetups

--- long-tutorial/src/pages/NewMeetUp.js
+++ long-tutorial/src/pages/NewMeetUp.js
@@ -1,5 +1,12 @@
+import NewMeetUpForm from "../components/meetups/NewMeetUpForm";
+
 function NewMeetUpsPage() {
-    return <div>New Meetups Page</div>
+    return (
+        <section>
+            <h1>Add New Meetup</h1>
+            <NewMeetUpForm />
+        </section>
+    )
 }
 
 export default NewMeetUpsPage;
